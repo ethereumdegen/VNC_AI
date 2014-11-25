f80b6c43714aaa3e0090acc534384e3a995f3d59
admazzola <admazzola@gmail.com>
2014-11-25 09:01:51 -0500

    Added OpenCV to buildpath, wrote code to bring the pixel array into an OpenCV matrix to run scripts on it

--- VNCAI/src/com/glavsoft/ai/Intel.java
+++ VNCAI/src/com/glavsoft/ai/Intel.java
@@ -5,6 +5,17 @@ import java.awt.Image;
 import java.awt.image.DataBufferInt;
 import java.awt.image.Raster;
 import java.awt.image.WritableRaster;
+import java.io.File;
+
+import org.omg.CORBA.Environment;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.highgui.Highgui;
+
+
 
 public class Intel {
 
@@ -12,7 +23,19 @@ public class Intel {
 	int[] pixels;
 	int mouseX;
 	int mouseY;
+	
+	
+	public Intel()
+	{
+		
 
+		
+	      
+	     
+	}
+	
+	
+boolean exported = false;
 	public void setRaster(WritableRaster raster) {
 		this.raster= raster;
 		
@@ -23,18 +46,36 @@ public class Intel {
 		int height = raster.getHeight();
 		int width = raster.getWidth();
 		
-		 int[][] result = new int[height][width];
 		
        // for (int pixel = 0, row = 0, col = 0; pixel < pixels.length; pixel += pixelLength) {
 		
-		//pixel index
+	
+		 
+
+
+		 System.loadLibrary( Core.NATIVE_LIBRARY_NAME );
+	     // Mat mat = Mat.eye( 3, 3, CvType.CV_8UC1 );
+	     // System.out.println( "mat = " + mat.dump() );
+	      		
+		//Java int is 32 bits signed, and there is only 1 channel here
+		Mat m = new Mat(height, width, CvType.CV_32SC1);
+	     m.put(0, 0, pixels);
+		Core.rectangle(m, new Point(150,150), new Point(550,550), new Scalar(0,255,0)); //draw rect on image
+		
+		if(!exported && mouseX > 450)
+		{
+		 String path =   System.getProperty("user.home") + "/Desktop";
+		Highgui.imwrite(path + "/opencvexport.jpg", m);
+		exported = true;
+		System.out.println("EXPORTED IMAGE");
+		}
 		 
 		
-		 
+		
 		int index = (mouseY*width + mouseX);
 		
-			if(index <0)
-				return;
+			if(index <0) return;//prevents crashing when mouse is out of bounds
+				
 		 
 		
 		int argb = pixels[index];
